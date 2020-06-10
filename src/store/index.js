98ad967a1e595b3471d841283d0b2a8bb99d333c
Allan James Lapid <ajLapid718@gmail.com>
2020-06-09 20:40:01 -0400

    partial setup of store

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+// Necessities and accessories for constructing our Redux store;
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Individual reducers altogether under an alias;
+
+// Construct our Redux store;
+const rootReducer = combineReducers(/* pass in reducers here */);
+const logger = createLogger({ collapsed: true });
+const middleware = composeWithDevTools(applyMiddleware(logger));
+const store = createStore(/* pass in rootReducer function here */); // 2nd arg: middleware;
+
+// Export our store by default, which will be provided to and injected within our entire application;
+export default store;
