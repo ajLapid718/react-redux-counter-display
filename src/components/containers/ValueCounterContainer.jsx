68d417e18f5f8fe91d42963a1f4e7ee3252ec706
Allan James Lapid <ajLapid718@gmail.com>
2020-06-09 20:27:13 -0400

    add instructions for component

--- src/components/containers/ValueCounterContainer.jsx
+++ src/components/containers/ValueCounterContainer.jsx
@@ -8,6 +8,12 @@ But...I still would like to have state management (Redux);
 
 Therefore, I will incorporate Redux in order to keep track of (and modify) the number value;
 
+If I am to use Redux, then I need to have this component connected to (subscription) the Redux store;
+
+This gives me a heads-up that indicates we will use connect() in this smart container;
+
+I will be about to read from (mapState) the Redux store and write to (mapDispatch) the Redux store;
+
 */
 
 class ValueCounterContainer extends Component {
