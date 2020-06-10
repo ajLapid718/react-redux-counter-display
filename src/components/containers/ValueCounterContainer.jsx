2725817b7ee0e64a6ee856d0e9850f4364443577
Allan James Lapid <ajLapid718@gmail.com>
2020-06-09 20:20:38 -0400

    add instructions for main component

--- src/components/containers/ValueCounterContainer.jsx
+++ src/components/containers/ValueCounterContainer.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 
+/*
+
+I want to keep my view layer as my view layer (ReactJS);
+
+But...I still would like to have state management (Redux);
+
+Therefore, I will incorporate Redux in order to keep track of (and modify) the number value;
+
+*/
+
 class ValueCounterContainer extends Component {
   constructor() {
     super();
